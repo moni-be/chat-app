2fd4b35aca415c0c274dd7c8e72cb71dc621f066
Moni <monica_be@hotmail.fr>
2020-03-31 10:46:20 +0200

    modifing status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,17 @@ function Contact(){
         <div className="Contact">         
             <img class="avatar"
               src="https://randomuser.me/api/portraits/women/76.jpg"
-              alt="Nelson Muntz" />      
-            <p class="name">Violet Ford</p> 
-            <div class="status status-text">
-              <span class="status status-online">online</span> 
-              <span class="status status-offline">offline</span> 
-            </div>
-      </div>
+              alt="Nelson Muntz" />
+        <div class="status status-text">   
+          <p class="name">Violet Ford </p>
+          <p class="status-text">
+            <span class="status-online"></span>
+            online
+        {/*   <span class="status status-offline">offline</span>  */}
+          </p>
+        </div> 
+
+      </div>  
     );
 
 }
